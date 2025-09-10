ad678ce88a502c5fb6cf8cf30aed5789188d02cf
P J MUHAMMED NAZEEL <115283508+pjmuhammednazeel@users.noreply.github.com>
2025-09-10 19:50:07 +0530

    Delete mini directory

--- mini/Admin/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import tbl_user
-
-admin.site.register(tbl_user)
--- mini/Admin/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('UserRegistration/', views.register_user, name='admin_hai'),
-    path('DisplayUser/', views.display_user, name='display_user'),]
